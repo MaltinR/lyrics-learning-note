8dffb7884bf7d98092cf5dfabb34e5c43a6b10db
Martin <martinlam@wisegrande.com>
2026-04-01 17:42:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Lyrics Learning Note
+
+### Version
+
++ Python - 3.12
---
+++ src/lyrics-learning-note/main.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI, APIRouter
+
+app = FastAPI()
+
+api_router = APIRouter(prefix="/api")
+
+@api_router.post("/songs")
+async def new_song():
+    return {"message": "WIP"}
+
+@api_router.put("/songs/{song_id}")
+async def update_song(song_id: str):
+    return {"message": f"{song_id} WIP"}
+
+@api_router.get("/songs/{song_id}")
+async def get_song(song_id: str):
+    return {"message": f"{song_id} WIP"}
+
+@api_router.post("/translate")
+async def translate_text():
+    return {"message": "WIP"}
+
+@api_router.post("/explain")
+async def explain_lyrics():
+    return {"message": "WIP"}
+
+@api_router.post("/tts")
+async def process_tts():
+    return {"message": "WIP"}
+
+@api_router.post("/lyrics")
+async def get_lyrics():
+    return {"message": "WIP"}
+
+@api_router.post("/lyrics/search")
+async def search_lyrics():
+    return {"message": "WIP"}
+
+app.include_router(api_router)
+
+# uvicorn main:app --port 3000
